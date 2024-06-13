597a7c002797bdb21a6f6f637de6d4b030b1c1f0
Maria Jose Juana Martinez <mjjm@MacBook-Pro-de-Maria.local>
2024-06-13 22:50:53 +0200

    corrección

--- idioma_cookie/visitas/config.php
+++ idioma_cookie/visitas/config.php
@@ -1,6 +1,14 @@
 <?php
-// configuración de las variables de página y expiración de la cookie
-$pagina = basename ($_SERVER['PHP_SELF']);
-$expiracion = time()+(30*24*60*60);
-
+if (isset($_COOKIE['visitas'])) 
+{ $visitas = $_COOKIE['visitas']+1; }
+else {
+    $visitas = 1;
+    setcookie('visitas', $visitas);
+}
+setcookie('visitas', $visitas);
+if (isset($_COOKIE['pagina']))
+{
+    $paginaAnt = $_COOKIE['pagina'];
+} else { $paginaAnt = 'Ongi Etorri';}
+setcookie('pagina', basename($_SERVER['PHP_SELF']), time() + 90*24*60*60);
 ?>
