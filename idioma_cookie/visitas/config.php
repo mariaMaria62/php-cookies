c41de22516c5141bde546959d51a9844711d20ad
MJose <mjjmar1962@gmail.com>
2024-06-13 11:16:46 +0200

    inicio

---
+++ idioma_cookie/visitas/config.php
@@ -0,0 +1,6 @@
+<?php
+// configuración de las variables de página y expiración de la cookie
+$pagina = basename ($_SERVER['PHP_SELF']);
+$expiracion = time()+(30*24*60*60);
+
+?>
---
+++ idioma_cookie/visitas/navegacion.php
@@ -0,0 +1,22 @@
+<?
+include 'config.php'
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contador de visitas</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Pagina 1</a> </li>
+            <li><a href="pagina2.php">Pagina 2</a> </li>
+            <li><a href="pagina3.php">Pagina 3</a> </li>
+        </ul>
+    </nav>
+    
+</body>
